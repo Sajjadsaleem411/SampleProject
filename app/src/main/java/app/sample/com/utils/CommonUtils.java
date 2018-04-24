ed97704dd40c766bed24a16a680e623a61484171
sajjadsaleem411 <muh.sajjadsaleem@gmail.com>
2018-04-24 14:10:25 +0500

    init commit

--- app/src/main/java/app/sample/com/utils/CommonUtils.java
+++ app/src/main/java/app/sample/com/utils/CommonUtils.java
@@ -81,19 +81,7 @@ public final class CommonUtils {
         return matcher.matches();
     }
 
-    public static String loadJSONFromAsset(Context context, String jsonFileName)
-            throws IOException {
 
-        AssetManager manager = context.getAssets();
-        InputStream is = manager.open(jsonFileName);
-
-        int size = is.available();
-        byte[] buffer = new byte[size];
-        is.read(buffer);
-        is.close();
-
-        return new String(buffer, "UTF-8");
-    }
 
 
 }
